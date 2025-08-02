20a255c6281d31b72081f569e5a5e7aac2f47c16
amit priyadarshi <amit.sinha.priyadarshi@gmail.com>
2025-08-02 13:22:38 +0530

    Document Analyzer Created

---
+++ src/document_analyzer/data_ingestion.py
@@ -0,0 +1,24 @@
+import os
+import fitz
+import uuid
+from datetime import datetime
+
+from logger.custom_logger import CustomLogger
+from exception.custom_exception import DocumentPortalException
+
+class DocumentHandler:
+    """
+    Handles PDF SAving and Reading Operations
+    Automatically logs all actions and support session-based organization
+    """
+
+    def __init__(self):
+        pass
+
+    def save_pdf(self):
+        pass
+
+    def read_pdf(self):
+        pass
+
+    
